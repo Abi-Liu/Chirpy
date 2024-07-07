d73bf4a1cc09f330ca32e6084f3f88139736e461
Abi Liu <abiliu018@gmail.com>
2024-07-07 13:50:41 -0700

    health check handler

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
-	server := http.Server{Addr: ":8080", Handler: mux}
+	mux.HandleFunc("/healthz", getHealthCheck)
+	server := &http.Server{Addr: ":8080", Handler: mux}
 
-	server.ListenAndServe()
+	fmt.Println("Server starting on port 8080")
+	log.Fatal(server.ListenAndServe())
+}
+
+func getHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	body, _ := json.Marshal("OK")
+	w.Write(body)
 }
