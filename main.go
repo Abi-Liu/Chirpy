b96b2b59016e3ac43b9336eddfa8eb0103959736
Abi Liu <abiliu018@gmail.com>
2024-07-07 14:36:19 -0700

    added stateful metrics endopint

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-
+	appConfig := &apiConfig{fileserverHits: 0}
+	mux.Handle("/app/", appConfig.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", getHealthCheck)
+	mux.HandleFunc("/metrics", appConfig.getMetrics)
+	mux.HandleFunc("/reset", appConfig.resetMetrics)
+
 	server := &http.Server{Addr: ":8080", Handler: mux}
 
 	fmt.Println("Server starting on port 8080")
@@ -19,7 +26,29 @@ func main() {
 }
 
 func getHealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	body, _ := json.Marshal("OK")
-	w.Write(body)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits++
+		fmt.Println(c.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (c *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	res := strconv.Itoa(c.fileserverHits)
+	w.Write([]byte("Hits: " + res))
+}
+
+func (c *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	c.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
